18ca4b24211d737a56963dca6b0e90288ecaccd5
Igor Bugaiov <no-reply>
2022-09-21 21:37:34 +0300

    st0-mod4.1-t2 1st try

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -1,6 +1,9 @@
 package school.mjc.stage0.conditions.task2;
 
+import jdk.jshell.spi.ExecutionControl;
+
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        System.out.println(first >= second ? first : second);
     }
 }
